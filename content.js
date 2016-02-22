70a8285b193fffc013aef71e3a6416b722f6dd0e
Santiago Quinteros <contacto.quinteros@gmail.com>
2016-02-22 13:19:57 -0300

    add dislikes button on MI

--- content.js
+++ content.js
@@ -7,26 +7,48 @@ function getVotes(shoutId,cb){
 }
 
 function renderButtons(){
-	$('.list-main-actions').each(function(){
-		var self = this;
-		var shoutId = $('.shout-action-like').attr('data-id').trim();
-		getVotes(shoutId,function(res,status){
-			var button = $(
-			`<li>
-				<a href="#" class="icon-pulgarabajo no-me-gusta" title="Votá negativo a este shout">
-					<span class="like_count">
-					${res?res.count:'0'}
-					</span>
-				</a>
-			</li>`);
-			$(self).append(button);
-			button.on('click', function(event,asd){
-				var likes = parseInt(button.find('span.like_count').text());
-				button.find('span.like_count').text(likes+1);
-				vote(shoutId);
+	if(window.location.pathname ==='/mi'){
+		$('.shout-footer > .s-action-list').each(function(){
+			var shoutId = $(this).attr('data-id').trim();
+			var self = this;
+			getVotes(shoutId,function(res,status){
+				var button = $(
+					`<div class="button-action-s pointer no-me-gusta" title="No me gusta">
+						<div class="action-number"> 
+							${res?'<span class="like_count">'+res.count+'</span>': '<span class="like_count"> 0 </span>' } 
+						</div>
+						<i class="icon-pulgarabajo"></i>
+					</div>`)
+				var test = $(self).children('.action-vote').before(button);
+				button.on('click', function(){
+					var likes = parseInt(button.find('span.like_count').text());
+					button.find('span.like_count').text(likes+1);
+					vote(shoutId);
+				});
+			});		
+		})	
+	}else{
+		$('.list-main-actions').each(function(){
+			var self = this;
+			var shoutId = $('.shout-action-like').attr('data-id').trim();
+			getVotes(shoutId,function(res,status){
+				var button = $(
+				`<li>
+					<a href="#" class="icon-pulgarabajo no-me-gusta" title="Votá negativo a este shout">
+						<span class="like_count">
+						${res?res.count:'0'}
+						</span>
+					</a>
+				</li>`);
+				$(self).append(button);
+				button.on('click', function(event,asd){
+					var likes = parseInt(button.find('span.like_count').text());
+					button.find('span.like_count').text(likes+1);
+					vote(shoutId);
+				});
 			});
-		});
-	})	
+		})	
+	}
 }
 
 function vote(shoutId){
